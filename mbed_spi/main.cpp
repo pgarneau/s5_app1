71f37335f3edd35c4d6c89cdef7a49b8bf737c4b
Philippe Garneau <garp2405@usherbrooke.ca>
2018-01-16 12:55:18 -0500

    PENIS

--- mbed_spi/main.cpp
+++ mbed_spi/main.cpp
@@ -1,56 +1,76 @@
 #include "mbed.h"
 #include "stdio.h"
 
+#define PI (3.1416)
 #define WAIT_TIME (1)
 #define I2C_READ_ADDRESS (0x3B)
 #define I2C_WRITE_ADDRESS (0x3A)
-#define UINT8_MAX (255)
+#define UINT14_MAX (16383)
 
 SPI spi(p5,NC,p7,p8);
 I2C i2c(p28, p27);
 
 int main() {
-	const char INIT_CMD[2] = {0x2A, 0x01};
-	const char x_msb[1] = {0x01};
-	const char x_lsb[1] = {0x02};
-	const char y_msb[1] = {0x03};
-	const char y_lsb[1] = {0x04};
+	char accel_data[2] = {0};
+	float z_offset = 0;
+	int16_t z_accel = 0;
+
+	const char init_cmd[2] = {0x2A, 0x01};
 	const char z_msb[1] = {0x05};
-	const char z_lsb[1] = {0x06};
 	
+	// Configuration de la lib SPI
 	spi.format(8,0);
-    spi.frequency(100000);
+  spi.frequency(100000);
 	spi.write(0x76);
 	
+	// Configuration I2C
 	i2c.frequency(100000);
-	char result[6] = {0}; //memset(result, '\0', sizeof(char)*1);
+
 	while (1) {
-		memset(result, 0, 1);
-		int write_1 = i2c.write(I2C_WRITE_ADDRESS, INIT_CMD, 2);
-		int write_2 = i2c.write(I2C_WRITE_ADDRESS, x_msb, 1, true);
-		uint8_t success = i2c.read(I2C_READ_ADDRESS, result, 6);
-		//for (uint8_t index = 0; index < 6; index++) {
-		//	uint8_t success = i2c.read(I2C_READ_ADDRESS, &result[index], 1);
-		//}
+		memset(accel_data, 0, 1);
+
+		// Vise registre z_msb et read en sequence de 2 pour MSB et LSB
+		i2c.write(I2C_WRITE_ADDRESS, z_msb, 1, true);
+		i2c.read(I2C_READ_ADDRESS, accel_data, 2);
 		
-		if (result[0] > UINT8_MAX/2) {
-			result[0] -= UINT8_MAX;
+		// Normalisation de la valeur de l'acceleration de Z
+    z_accel = (accel_data[0] << 6) | (accel_data[1] >> 2);
+    if (z_accel > UINT14_MAX/2)
+        z_accel -= UINT14_MAX;
+		
+		float z_accel_result = (z_accel/4096.0);
+		
+		if (z_accel_result > 1) {
+			z_accel_result = 1;
+		}
+		if (z_accel_result < -1) {
+			z_accel_result = -1;
 		}
 		
-		int8_t x_result = (result[0] * 365) / 255;
-		int8_t y_result = result[2] / 10;
-		int8_t z_result = result[4] / 10;
+		// Transformation valeur d'accel en z en angle
+		float z_angle = std::acos(z_accel_result);
 		
-		char string_result[3] = {0};
+		// Assurer que l'angle est max 90 degree
+		if (z_angle > PI / 2) {
+			z_angle = PI - z_angle;
+		}
+		
+		// Transformation rad en degree
+		z_angle = z_angle * 180 / PI;
+		
+		// Remplissage tableau de char pour afficher valeur angle
+		char string_result[3] = {0};		
+		uint8_t string_size = 0;
+	  string_size = sprintf(string_result, "%d",(int)z_angle - (int)z_offset);
 				
-		uint8_t banane = 0;
-	    banane = sprintf(string_result, "%d", x_result);
+		// Afficher angle
 		spi.write(0x76);
-		for (int index = 0; index < banane; index++) {
+		for (int index = 0; index < string_size; index++) {
 			spi.write(0x79);
-			spi.write(4 - banane + index);
+			spi.write(4 - string_size + index);
 			spi.write(string_result[index]);
 		}
+		
 		wait(0.5);
 	}
 }
--- mbed_uart/main.cpp
+++ mbed_uart/main.cpp
@@ -1,6 +1,8 @@
 #include "mbed.h"
 #include "stdio.h"
 
+// Constantes necessaires pour code
+#define PI (3.1416)
 #define SBIT_WordLength (0x00)
 #define SBIT_DLAB (0x07)
 #define SBIT_FIFO (0x00)
@@ -10,8 +12,9 @@
 #define I2C_READ_ADDRESS (0x3B)
 #define I2C_WRITE_ADDRESS (0x3A)
 #define DLM_VALUE (0x00)
-#define UINT8_MAX (255)
+#define UINT14_MAX (16383)
 
+// Formateur de valeur hexa en binaire jusqu'a 32 bits
 #define PRINTF_BINARY_PATTERN_INT8 "%c%c%c%c%c%c%c%c"
 #define PRINTF_BYTE_TO_BINARY_INT8(i)    \
     (((i) & 0x80ll) ? '1' : '0'), \
@@ -32,13 +35,15 @@
 #define PRINTF_BYTE_TO_BINARY_INT32(i) \
     PRINTF_BYTE_TO_BINARY_INT16((i) >> 16), PRINTF_BYTE_TO_BINARY_INT16(i)
 	
+// Configuration comm i2c
 I2C i2c(p28, p27);
 
 int main() {
 	const char init_cmd[2] = {0x2A, 0x01};
 	const char x_msb[1] = {0x01};
+	const char z_msb[1] = {0x05};
 	
-	// Configuration I2C
+	// Configuration I2C + initialisation
 	i2c.frequency(100000);
 	i2c.write(I2C_WRITE_ADDRESS, init_cmd, 2);
 	
@@ -74,51 +79,215 @@ int main() {
 	*(volatile unsigned int*)0x4002C040 = *(volatile unsigned int*)0x4002C040 | 0xC0000000;
 	
 	// GPIO PinMode
-	*(volatile unsigned int*)0x4002C044 = *(volatile unsigned int*)0x4002C044 & ~0x00F00000;
-	*(volatile unsigned int*)0x4002C044 = *(volatile unsigned int*)0x4002C044 | 0x00300000;
+	*(volatile unsigned int*)0x4002C044 = *(volatile unsigned int*)0x4002C044 & ~0x00FF0000;
+	*(volatile unsigned int*)0x4002C044 = *(volatile unsigned int*)0x4002C044 | 0x003F0000;
 	
 	// GPIO FIO0PIN3
 	*(volatile unsigned int*)0x2009C003 = *(volatile unsigned int*)0x2009C003 | 0x00;
 
-	char accel_data[6] = {0};
-	uint8_t z_offset = 0;
+	// Variables necessaires durant la boucle
+	char accel_data[2] = {0};
+	float z_offset = 0;
+	int16_t z_accel = 0;
+	
+	uint8_t low_counter = 0;
+	char protocol_data[7] = {0};
+	uint8_t data_counter = 0;
+	bool reading_state = false;
+	uint8_t parity_counter = 0;
+	uint8_t decimal_control = 0x00;
 	while (1) {
 		memset(accel_data, 0, 1);
 
 		// Vise registre x_msb et read en sequence
-		i2c.write(I2C_WRITE_ADDRESS, x_msb, 1, true);
-		i2c.read(I2C_READ_ADDRESS, accel_data, 6);
+		i2c.write(I2C_WRITE_ADDRESS, z_msb, 1, true);
+		i2c.read(I2C_READ_ADDRESS, accel_data, 2);
 		
-		for (int index = 0; index < 5; index += 2) {
-			if (accel_data[index] > UINT8_MAX/2) {
-				accel_data[index] -= UINT8_MAX;
-			}
+		// Calcul du vecteur d'acceleration en z + normalisation
+    z_accel = (accel_data[0] << 6) | (accel_data[1] >> 2);
+    if (z_accel > UINT14_MAX/2)
+        z_accel -= UINT14_MAX;
+		
+		float z_accel_result = (z_accel/4096.0);
+		
+		if (z_accel_result > 1) {
+			z_accel_result = 1;
+		}
+		if (z_accel_result < -1) {
+			z_accel_result = -1;
 		}
 		
-		int8_t x_result = (accel_data[0] * 360) / 255;
-		int8_t y_result = (accel_data[2] * 360) / 255;
-		int8_t z_result = (accel_data[4] * 360) / 255;
-		z_result = 90 - z_result;
+		// Transformation de l'accel en angle
+		float z_angle = std::acos(z_accel_result);
+		
+		// S'assure que ca reste entre 0 et 90
+		if (z_angle > PI / 2) {
+			z_angle = PI - z_angle;
+		}
+		
+		// Transforme rad en degree
+		z_angle = z_angle * 180 / PI;
 		
 		// Verification si bouton appuye pour remise a 0
 		char binary_result[32] = {0};
 		sprintf(binary_result,PRINTF_BINARY_PATTERN_INT32,PRINTF_BYTE_TO_BINARY_INT32(*(volatile unsigned int*)0x2009C017));
 		if (binary_result[29] == '1') {
-			z_offset = z_result;
+			z_offset = z_angle;
 		}
 		
+		// Transformation hex a tableau de char pour affichage
 		char string_result[3] = {0};		
 		uint8_t string_size = 0;
-	    string_size = sprintf(string_result, "%d", z_result - z_offset);
+	  string_size = sprintf(string_result, "%d",(int)z_angle - (int)z_offset);
 		
-		// Afficher angle
-		*(volatile unsigned int*)0x40010000 = 0x76;
-		for (int index = 0; index < string_size; index++) {
-			*(volatile unsigned int*)0x40010000 = 0x79;
-			*(volatile unsigned int*)0x40010000 = (4 - string_size + index);
-			*(volatile unsigned int*)0x40010000 = (string_result[index]);
+		// Detection d'un premier high sur la clock apres une periode de stop
+		if (binary_result[31] == '1' && low_counter > 6) {
+			reading_state = true;
+			
+			protocol_data[data_counter] = binary_result[30];
+			data_counter++;
 		}
 		
-		wait(0.5);
+		// Detection de low sur clock
+		else if (binary_result[31] == '0') {
+			//memset(protocol_data, 0, 7);
+			reading_state = false;
+			low_counter++;
+		}
+		
+		// Si notre data est "rempli"
+		if (data_counter == 7) {
+			low_counter = 0;
+			data_counter = 0;
+			parity_counter = 0;
+			char command = protocol_data[5];
+			char bit2 = protocol_data[4];
+			char bit1 = protocol_data[3];
+			char bit0 = protocol_data[2];
+			char parity = protocol_data[1];
+			
+			// Verification que le premier bit et dernier bit est 1 pour que le contenu soit valide
+			if (protocol_data[0] == '1' && protocol_data[6] == '1') {
+				// Calcul de parite
+				for (uint8_t index = 1; index < 6; index++) {
+					if (protocol_data[index] == '1') {
+						parity_counter++;
+					}
+				}
+				// Assurance que parite est valide dans le data
+				if (parity_counter % 2 == 0) {
+					// Traduction des commandes du guide etudiant en code pour UART
+					if (bit0 == '0' && bit1 == '0' && bit2 == '0') {
+						if (command == '1') {
+							decimal_control &= ~0x0F;
+							decimal_control |= 0x01;
+						} else {
+							decimal_control &= ~0x0F;
+							decimal_control |= 0x00;
+						}
+						*(volatile unsigned int*)0x40010000 = 0x77;
+						*(volatile unsigned int*)0x40010000 = decimal_control;
+					}
+					else if (bit0 == '0' && bit1 == '0' && bit2 == '1') {
+						if (command == '1') {
+							decimal_control &= ~0x0F;
+							decimal_control |= 0x02;
+						} else {
+							decimal_control &= ~0x0F;
+							decimal_control |= 0x00;
+						}
+						*(volatile unsigned int*)0x40010000 = 0x77;
+						*(volatile unsigned int*)0x40010000 = decimal_control;
+					}
+					else if (bit0 == '0' && bit1 == '1' && bit2 == '0') {
+						if (command == '1') {
+							decimal_control &= ~0x0F;
+							decimal_control |= 0x04;
+						} else {
+							decimal_control &= ~0x0F;
+							decimal_control |= 0x00;
+						}
+						*(volatile unsigned int*)0x40010000 = 0x77;
+						*(volatile unsigned int*)0x40010000 = decimal_control;
+					}
+					else if (bit0 == '0' && bit1 == '1' && bit2 == '1') {
+						if (command == '1') {
+							decimal_control &= ~0x0F;
+							decimal_control |= 0x08;
+						} else {
+							decimal_control &= ~0xF0;
+							decimal_control |= 0x00;
+						}
+						*(volatile unsigned int*)0x40010000 = 0x77;
+						*(volatile unsigned int*)0x40010000 = decimal_control;
+					}
+					else if (bit0 == '1' && bit1 == '0' && bit2 == '0') {
+						if (command == '1') {
+							decimal_control &= ~0xF0;
+							decimal_control |= 0x10;
+						} else {
+							decimal_control &= ~0xF0;
+							decimal_control |= 0x00;
+						}
+						*(volatile unsigned int*)0x40010000 = 0x77;
+						*(volatile unsigned int*)0x40010000 = decimal_control;
+					}
+					else if (bit0 == '1' && bit1 == '0' && bit2 == '1') {
+						if (command == '1') {
+							decimal_control &= ~0xF0;
+							decimal_control |= 0x20;
+						} else {
+							decimal_control &= ~0xF0;
+							decimal_control |= 0x00;
+						}
+						*(volatile unsigned int*)0x40010000 = 0x77;
+						*(volatile unsigned int*)0x40010000 = decimal_control;
+
+					}
+					else if (bit0 == '1' && bit1 == '1' && bit2 == '0') {
+						if (command == '1') {
+							*(volatile unsigned int*)0x40010000 = 0x7A;
+							*(volatile unsigned int*)0x40010000 = 0x64;
+						} else {
+							*(volatile unsigned int*)0x40010000 = 0x7A;
+							*(volatile unsigned int*)0x40010000 = 0xF;
+						}
+					}
+				}
+			}
+		}
+		
+		// Afficher angle
+		memset(binary_result, 0, 32);
+		sprintf(binary_result,PRINTF_BINARY_PATTERN_INT32,PRINTF_BYTE_TO_BINARY_INT32(*(volatile unsigned int*)0x40010014));
+		if (binary_result[26] == '1') {
+			//*(volatile unsigned int*)0x40010000 = 0x76;
+			// Remettre afficheur vide sur chiffres
+			*(volatile unsigned int*)0x40010000 = 0x7B;
+			*(volatile unsigned int*)0x40010000 = 0x00;
+			*(volatile unsigned int*)0x40010000 = 0x7C;
+			*(volatile unsigned int*)0x40010000 = 0x00;
+			*(volatile unsigned int*)0x40010000 = 0x7D;
+			*(volatile unsigned int*)0x40010000 = 0x00;
+			*(volatile unsigned int*)0x40010000 = 0x7E;
+			*(volatile unsigned int*)0x40010000 = 0x00;
+			// Afficher en ordre les valeurs
+			for (int index = 0; index < string_size; index++) {
+				*(volatile unsigned int*)0x40010000 = 0x79;
+				*(volatile unsigned int*)0x40010000 = (4 - string_size + index);
+				*(volatile unsigned int*)0x40010000 = (string_result[index]);
+			}
+		}
+		
+		// Synchronisation pour lire le data
+		if (low_counter > 0) {
+			wait(0.002);
+		}
+		else if (reading_state) {
+			wait(0.004);
+		}
+		else {
+			wait(0.002);
+		}
 	}
 }
